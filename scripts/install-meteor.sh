1424eafcf09125403a135a75974b419f427460dc
Jeremy Shimko <jeremy.shimko@gmail.com>
2016-10-28 02:27:45 -0400

    fix permissions with Meteor install

--- scripts/install-meteor.sh
+++ scripts/install-meteor.sh
@@ -5,7 +5,7 @@ set -e
 if [ "$DEV_BUILD" = true ]; then
   # if this is a devbuild, we don't have an app to check the .meteor/release file yet,
   # so just install the latest version of Meteor
-  gosu node curl https://install.meteor.com/ | sed 's:$HOME:/home/node:g' | sh
+  curl https://install.meteor.com/ | sed 's:$HOME:/home/node:g' | sh
 else
   # download installer script
   curl https://install.meteor.com -o /home/node/install_meteor.sh
@@ -21,9 +21,8 @@ else
 
   # install
   printf "\n[-] Installing Meteor $METEOR_VERSION...\n\n"
-  chown node /home/node/install_meteor.sh
-  gosu node sh /home/node/install_meteor.sh
+  sh /home/node/install_meteor.sh
 fi
 
-# copy the executable into the $PATH
-cp /home/node/.meteor/packages/meteor-tool/$METEOR_VERSION/mt-os.linux.x86_64/scripts/admin/launch-meteor /usr/bin/meteor
+# fix permissions
+chown -R node /home/node/.meteor
