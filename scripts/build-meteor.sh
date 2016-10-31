98a24f53753b371a48fb272b8616264ee1460f75
Jeremy Shimko <jeremy.shimko@gmail.com>
2016-10-31 14:09:45 -0400

    add better logging for meteor build debugging

--- scripts/build-meteor.sh
+++ scripts/build-meteor.sh
@@ -5,17 +5,20 @@
 #
 set -e
 
-printf "\n[-] Building Meteor application...\n\n"
-
 cd $APP_SOURCE_DIR
 
 # Install app deps
+printf "\n[-] Running npm install...\n\n"
 meteor npm install
 
-# build the source
+# build the bundle
+printf "\n[-] Building Meteor application...\n\n"
 mkdir -p $APP_BUNDLE_DIR
 chown -R node $APP_BUNDLE_DIR
 meteor build --unsafe-perm --directory $APP_BUNDLE_DIR
+
+# run npm install in bundle
+printf "\n[-] Running npm install in the server bundle...\n\n"
 cd $APP_BUNDLE_DIR/bundle/programs/server/
 meteor npm install --production
 
