1472b09ab01ff0615315c315959cfbe4c5cd5792
Jeremy Shimko <jeremy.shimko@gmail.com>
2017-05-08 12:27:41 -0400

    replace tar with bsdtar in Meteor installer (fix #39)

--- scripts/install-meteor.sh
+++ scripts/install-meteor.sh
@@ -17,6 +17,10 @@ else
   # set the release version in the install script
   sed -i.bak "s/RELEASE=.*/RELEASE=\"$METEOR_VERSION\"/g" /tmp/install_meteor.sh
 
+  # replace tar command with bsdtar in the install script (bsdtar -xf "$TARBALL_FILE" -C "$INSTALL_TMPDIR")
+  # https://github.com/jshimko/meteor-launchpad/issues/39
+  sed -i.bak "s/tar -xzf.*/bsdtar -xf \"\$TARBALL_FILE\" -C \"\$INSTALL_TMPDIR\"/g" /tmp/install_meteor.sh
+
   # install
   printf "\n[-] Installing Meteor $METEOR_VERSION...\n\n"
   sh /tmp/install_meteor.sh
