ea15e1389f35935d764b65729e81825324fab804
Jeremy Shimko <jeremy.shimko@gmail.com>
2017-06-01 01:22:03 -0400

    get curl

--- .circleci/install.sh
+++ .circleci/install.sh
@@ -2,6 +2,10 @@
 
 set -x
 
+# get curl
+apt-get update
+apt-get install -y curl
+
 # install Docker client
 curl -L -o /tmp/docker-$DOCKER_VERSION.tgz https://get.docker.com/builds/Linux/x86_64/docker-$DOCKER_VERSION.tgz
 tar -xz -C /tmp -f /tmp/docker-$DOCKER_VERSION.tgz
