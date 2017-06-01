01144aa2723c925fd8456d0cc4d5a71178e04b74
Jeremy Shimko <jeremy.shimko@gmail.com>
2017-06-01 00:51:16 -0400

    update build script

--- build.sh
+++ build.sh
@@ -6,6 +6,5 @@ IMAGE_NAME=${1:-"jshimko/meteor-launchpad"}
 
 printf "\n[-] Building $IMAGE_NAME...\n\n"
 
-docker build -t $IMAGE_NAME:base .
 docker build -f dev.dockerfile -t $IMAGE_NAME:devbuild .
-docker build -f prod.dockerfile -t $IMAGE_NAME:latest .
+docker build -t $IMAGE_NAME:latest .
