d35ba4f009e3ee01298acc8b13889d300cf78545
Jeremy Shimko <jeremy.shimko@gmail.com>
2017-09-12 14:40:36 -0400

    update usage comments in deploy script

--- deploy.sh
+++ deploy.sh
@@ -2,10 +2,13 @@
 
 set -e
 
+# Example usage:
+# deploy.sh jshimko/meteor-launchpad v1.0.0
+
 IMAGE_NAME=$1 # jshimko/meteor-launchpad
 VERSION=$2    # v1.0.0
 
-# create a versioned tags
+# create versioned tags
 docker tag $IMAGE_NAME:devbuild $IMAGE_NAME:$VERSION-devbuild
 docker tag $IMAGE_NAME:latest $IMAGE_NAME:$VERSION
 
