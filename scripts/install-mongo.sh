dba31d9f0732de4829e44548bf754488ad2b84ea
johnnyutahio <ciaran@johnnyutah.io>
2019-03-27 14:04:32 -0600

    updated install-mongo.sh to refer to stretch

--- scripts/install-mongo.sh
+++ scripts/install-mongo.sh
@@ -11,7 +11,7 @@ if [ "$INSTALL_MONGO" = true ]; then
 
 	apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 0C49F3730359A14518585931BC711F9BA15703C6
 
-  echo "deb http://repo.mongodb.org/apt/debian jessie/mongodb-org/$MONGO_MAJOR main" > /etc/apt/sources.list.d/mongodb-org.list
+  echo "deb http://repo.mongodb.org/apt/debian stretch/mongodb-org/$MONGO_MAJOR main" > /etc/apt/sources.list.d/mongodb-org.list
 
 	apt-get update
 
