defa2ad1eb45b267927a6e6b3dc2b86f18dacdd9
Daniel Gustaw <gustaw.daniel@gmail.com>
2021-09-10 18:09:06 +0200

    Probaly fixed bsdtar problem

--- scripts/install-meteor.sh
+++ scripts/install-meteor.sh
@@ -19,7 +19,7 @@ else
 
   # replace tar command with bsdtar in the install script (bsdtar -xf "$TARBALL_FILE" -C "$INSTALL_TMPDIR")
   # https://github.com/jshimko/meteor-launchpad/issues/39
-  sed -i.bak "s/tar -xzf.*/bsdtar -xf \"\$TARBALL_FILE\" -C \"\$INSTALL_TMPDIR\"/g" /tmp/install_meteor.sh
+  sed -i.bak "s/tar -xzf.*/tar -xf \"\$TARBALL_FILE\" -C \"\$INSTALL_TMPDIR\"/g" /tmp/install_meteor.sh
 
   # install
   printf "\n[-] Installing Meteor $METEOR_VERSION...\n\n"
