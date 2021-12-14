ea6ce13215146a0bdcf805901b90d9f57a009bf7
Daniel Gustaw <daniel.gustaw@acaisoft.com>
2021-12-14 10:47:03 +0100

    Updated debian version in dev.docker + added nointeractive frontedn on debian image

--- scripts/install-deps.sh
+++ scripts/install-deps.sh
@@ -21,7 +21,7 @@ if [ -f $APP_SOURCE_DIR/launchpad.conf ]; then
   fi
 fi
 
-apt-get install -y --no-install-recommends curl bzip2 bsdtar build-essential python git wget
+apt-get install -y --no-install-recommends curl bzip2 libarchive-tools build-essential python git wget
 
 
 # install gosu
