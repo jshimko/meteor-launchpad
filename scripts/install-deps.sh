186e4de140215a6886a74ea06c1358bb3eb70bea
Daniel Gustaw <gustaw.daniel@gmail.com>
2023-11-05 22:58:33 +0400

    conda installed to provide python2

--- scripts/install-deps.sh
+++ scripts/install-deps.sh
@@ -20,9 +20,27 @@ if [ -f $APP_SOURCE_DIR/launchpad.conf ]; then
   fi
 fi
 
-apt-get install -y --no-install-recommends curl bzip2 libarchive-tools build-essential  git wget  gnupg2 dirmngr
+apt-get install -y --no-install-recommends curl bzip2 libarchive-tools build-essential git wget gpg gnupg2 dirmngr
 
+### Conda
 
+# Install our public GPG key to trusted store
+curl https://repo.anaconda.com/pkgs/misc/gpgkeys/anaconda.asc | gpg --dearmor > conda.gpg
+install -o root -g root -m 644 conda.gpg /usr/share/keyrings/conda-archive-keyring.gpg
+
+# Check whether fingerprint is correct (will output an error message otherwise)
+gpg --keyring /usr/share/keyrings/conda-archive-keyring.gpg --no-default-keyring --fingerprint 34161F5BF5EB1D4BFBBB8F0A8AEB4F8B29D82806
+
+# Add our Debian repo
+echo "deb [arch=amd64 signed-by=/usr/share/keyrings/conda-archive-keyring.gpg] https://repo.anaconda.com/pkgs/misc/debrepo/conda stable main" > /etc/apt/sources.list.d/conda.list
+
+apt-get update
+apt-get install conda
+
+source /opt/conda/etc/profile.d/conda.sh
+
+conda create --name py2 python=2.7
+conda activate py2
 # install gosu
 
 dpkgArch="$(dpkg --print-architecture | awk -F- '{ print $NF }')"
